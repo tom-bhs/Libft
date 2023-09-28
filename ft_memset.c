0d36d2a83016695323fafcd93c03d757951631f3
Tom BIHOUES <Tom.bihouess@gmail.com>
2023-09-28 14:06:39 +0200

    42

---
+++ ft_memset.c
@@ -0,0 +1,18 @@
+#include <string.h>
+
+void *ft_memset(void *b, int c, size_t len)
+{
+    size_t     i;
+
+    if (!b)
+        return (NULL);
+
+    i = 0;
+    while (i < len)
+    {
+            *(unsigned char*)(b + i) = (unsigned char)c;
+            i++;
+    }
+    return (b);
+
+}
