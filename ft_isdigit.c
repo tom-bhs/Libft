8ff6e351332c2ce21ccb24a047100b7cdecf20aa
Tom Bihoues <tbihoues@c1r3p3.42lehavre.fr>
2023-10-19 18:05:32 +0200

    42

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,14 +6,14 @@
 /*   By: tbihoues <tbihoues@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 18:17:51 by tbihoues          #+#    #+#             */
-/*   Updated: 2023/10/19 17:15:23 by tbihoues         ###   ########.fr       */
+/*   Updated: 2023/10/19 17:30:29 by tbihoues         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //#include "libft.h"
 #include <stdio.h>
 
-int		ft_isdigit(int c)
+int	ft_isdigit(int c)
 {
 	return (c >= '0' && c <= '9');
 }
