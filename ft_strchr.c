93627d1bc0fec52f05f61b3c2fa30b0daffbb733
Tom Bihoues <tbihoues@c1r6p8.42lehavre.fr>
2023-10-20 23:56:19 +0200

    42

--- ft_strchr.c
+++ ft_strchr.c
@@ -9,3 +9,20 @@
 /*   Updated: 2023/10/19 17:14:58 by tbihoues         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+		{
+			return ((char *)s + i);
+		}
+		i++;
+	}
+	return (NULL);
+}
